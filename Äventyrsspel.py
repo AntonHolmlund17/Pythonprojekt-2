86893807b8997a8b546f05e7df2041723e7b5250
AntonHolmlund17 <152483711+AntonHolmlund17@users.noreply.github.com>
2024-05-02 10:33:26 +0200

    Update Äventyrsspel.py

--- Äventyrsspel.py
+++ Äventyrsspel.py
@@ -5,7 +5,7 @@ print(""" Welcome to the dungeon adventurer! Your goal is to survive through 12 
 1. Go to the next level
 2. Check your stats
 3. Look in your backpack
- 
+
 By going through to the next level you are going to choose one out of three doors. Behind each door there is a chance of finding a monster, falling in a trap or finding a chest with valuable items.""")
 
 
@@ -102,14 +102,14 @@ traps = [spike_trap, poison_trap, boulder_trap, fall_trap]
 
 
 
-#  Skriva funktioner för att kunna visa ryggsäcken och spelarens stats
 #  Skriva felinput hantering
 #  Kolla vs code för lösning på inventory
 
 # Funktionen visar inventory
 
-
+# FUNKAR INTE FIXA DEN
 def swap(item, slot):
+    backpack.remove(slot)
     backpack.insert(slot, item)
 
 # Funktion för att plocka upp item från kista och byta ut -------------------
@@ -117,33 +117,35 @@ def swap(item, slot):
 def pick_up_item(item):
   print("This is what your backpack currently looks like")
   show_backpack()
-  
+
   print("What slot would you like to put this item in?")
   slot = int(input(": "))
-  
+
   while slot < 1 or slot > 5:
     print("Please enter a number between 1 and 5")
     slot = int(input(": "))
 
   swap(item, slot-1)
 
-# Funktion för att printa ut backpack när man vill se det ------------------------------
-  
+# Funktion för att printa ut backpack  --------------------------------------------------
+
 def show_backpack(): 
     print("\nYou take off your backpack and look inside. Inside you see:")
-    print("\nNAME      STRE  HP")
+    print("\nNAME  STRENGTH  HP")
     for n in range(5):
         print(f"{backpack[n].item_name:12} {backpack[n].item_stre} {backpack[n].item_hp:3}")
+
+    input(f"\nPress enter to continue")
 
 
 
 # Funktionen ska fråga vad spelaren vill göra, inventory, backpack eller ett nytt rum. 
 
 def first_choice(): 
-   
+
   print("""
 
-  
+
     What do you want to do? 
     1. Go to the next level 
     2. Check your stats     
@@ -151,7 +153,7 @@ def first_choice():
 
   while True:
     choice = input("\nWrite here: ")
-    
+
     if choice == "1" or choice == "next level":
       return random_room()
     elif choice == "2" or choice == "stats":
@@ -166,7 +168,7 @@ def first_choice():
 
 def monster():
   print(f"\n\n\nYou opened the door and found a monsters nest.") 
-  
+
   if player.lvl <= 4:
     monster = random.choice(early_monsters)
   elif player.lvl <= 8:
@@ -176,27 +178,29 @@ def monster():
 
   print(f"\nYou have encountered a {monster.monster_name}!")
   input("\nPress enter to fight!")
-  
+
   if player.stre >= monster.monster_stre:
     print("\nYou have defeated the monster and didn't lose any health!")
+
   elif player.stre < monster.monster_stre:
     print("\nYou have lost some health!")
     player.hp -= monster.monster_stre - player.stre
     print(f"\nYou have {player.hp} health left!")
+
   elif player.hp + player.stre <= monster.stre:
     print("\nThe monster defeated you!")
     player.hp == 0
-    
-    
+
+
 #  Funktion för att slumpa fälla ------------------
 
 def trap():
   print("\n\n\nYou opened the door and found a trap") 
   trap = random.choice(traps)
-  
+
   print(f"\nYou have encountered a {trap.trap_name}!")
   player.hp -= trap.trap_stre
-  
+
   print(f"\nYou have {player.hp} health left!")
 
 
@@ -204,17 +208,17 @@ def trap():
 
 def item():
   print("\n \n \nYou opened the door and found a chest.")
-  
+
   item = random.choice(items)
   print(f"\nYou have found a {item.item_name}!")
 
   choice = input("\nDo you want to pick it up? Yes or No: ")
-  
+
   while choice.lower() != "yes"  and choice.lower() != "no":
     print("\nWrong input, try again.")
     print("\nPlease answer Yes or No")
     choice = input("\nDo you want to pick it up? Yes or No: ")
-    
+
   if choice.lower() == "yes":
     pick_up_item(item)
   elif choice.lower() == "no":
@@ -222,7 +226,7 @@ def item():
 
 
     # Funktion för att bestäma vad som är bakom dörren ---------- 
-  
+
 def room_choice(a):
     if a == 1:
       monster()
@@ -231,20 +235,20 @@ def room_choice(a):
     elif a == 3:
       item()
 
-  
+
 def random_room():
   room_number = random.randint(1, 3)
-  
+
   print("""
   In front of you there are 3 doors, behind one is a trap, the other is a chest and behind the third door is a monster. 
   Choose between door 1, 2 or 3.""") 
-  
+
   door_choice = input("\nWhich door do you choose? ")
-  
+
   while door_choice != "1" and door_choice != "2" and door_choice != "3": 
     print("Wrong input, try again") 
     door_choice = input("\nWhich door do you choose? ")
-  
+
   room_choice(room_number)
 
 # Kod som kör spelet
